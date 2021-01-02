b6984dbfaaed7138a1300d60b8f29935b2a1f70d
2439905184 <suchenglong018@126.com>
2021-01-02 14:29:06 +0800

    添加了更多游戏界面，的跳转到但对应游戏的功能，添加了一个自动点击的功能，预计对接其他广告联盟 已经把20几个句子全部写完 下一步，研究sdk对接

--- src/main/java/com/littesandbox/clicksandbox/MainActivity.java
+++ src/main/java/com/littesandbox/clicksandbox/MainActivity.java
@@ -19,7 +19,36 @@ import android.app.AlertDialog;
 import android.os.Build;
 import android.app.Dialog;
 import android.content.DialogInterface;
+/*所有的句子
 
+ 1道可道，非常道
+ 2.有空去看看《国际歌》，不要忘记了前人的努力
+ 3.上学时，不要整天想着玩游戏，等放了假，随便玩。
+ 4.如果感到焦虑不安，可以试试正念疗法。
+ 5.学历代表过去，学习能力代表将来
+ 6.生于忧患，死于安乐
+ 7.我的世界是一款自由的沙盒游戏，你可以试试
+ 8.游戏是世界第九大艺术
+ 9.金坷垃是检验神曲的唯一标准
+ 10.github是全球最大的软件技术开源平台
+ 11.在《光遇》遇见每一个温柔的人
+ 12.一根葱这种零食挺好吃的
+ 13.如果对网页制作感兴趣，可以试试easypage
+ 14.真正有目标的人，不会整天看电视，只有闲人才会从早看到晚
+ 15.学历不能代表一切，没有高学历，一样可以从事自己喜欢的事情并去开公司，只是会更加艰难一点。
+ 16.读书不能死读书，要活学活用
+ 17.身体简况是做其他事情的基础条件
+ 18.要做大事，先从小事做起
+ 19.在追求梦想的时候，不能被资本腐蚀了心志，要明白你转来的钱都是广大劳动人民的。
+ 20学习任何东西，不管是自学还是老师教，都要记得做笔记，如果不练习，不使用，很快就会遗忘。
+ 21.如果觉得思维混乱，可以试试思维导图
+ 22.透写台是个好工具，可以帮助你更好地临摹
+ 23.到了初中以后，可以思考下学习是为了什么，不要变成学习机器，不能过于功利主义
+ 24.字典上的字都是互相解释的
+ 25.赚钱不能吃香太难看，不能恶心人，要取一种平衡状态
+ 26.是金子总会发光
+ 27.音乐最擅长表达的是情绪，但不擅长表达理性逻辑
+ 28.出名的方式有很多种，出臭名还是出美名，我的观点是出美名更好*/
 public class MainActivity extends Activity
 {
 	//句子索引
@@ -29,7 +58,7 @@ public class MainActivity extends Activity
 	 TextView randView;
 	 TextView unlocked_stn;
 	 ListView list;
-	 String sentences[]={"靠天靠地不如靠自己","是金子总会发光"};
+	 String sentences[]={"道可道，非常道","有空去看看《国际歌》，不要忘记了前人的努力","上学时，不要整天想着玩游戏，等放了假，随便玩。",".如果感到焦虑不安，可以试试正念疗法。","学历代表过去，学习能力代表将来","生于忧患，死于安乐","我的世界是一款自由的沙盒游戏，你可以试试","游戏是世界第九大艺术","金坷垃是检验神曲的唯一标准","github是全球最大的软件技术开源平台","在《光遇》遇见每一个温柔的人","一根葱这种零食挺好吃的","如果对网页制作感兴趣，可以试试easypage","真正有目标的人，不会整天看电视，只有闲人才会从早看到晚","学历不能代表一切，没有高学历，一样可以从事自己喜欢的事情并去开公司，只是会更加艰难一点。","读书不能死读书，要活学活用","身体简况是做其他事情的基础条件","要做大事，先从小事做起","在追求梦想的时候，不能被资本腐蚀了心志，要明白你转来的钱都是广大劳动人民的。","学习任何东西，不管是自学还是老师教，都要记得做笔记，如果不练习，不使用，很快就会遗忘","如果觉得思维混乱，可以试试思维导图",".透写台是个好工具，可以帮助你更好地临摹","到了初中以后，可以思考下学习是为了什么，不要变成学习机器，不能过于功利主义","字典上的字都是互相解释的","赚钱不能吃香太难看，不能恶心人，要取一种平衡状态","是金子总会发光","音乐最擅长表达的是情绪，但不擅长表达理性逻辑","出名的方式有很多种，出臭名还是出美名，我的观点是出美名更好"};
 	 TextView tmptext;
 	 ArrayList<String> stn=new ArrayList<String>();
 	 Context ctx;
@@ -142,9 +171,19 @@ public class MainActivity extends Activity
 		Intent i=new Intent(Intent.ACTION_VIEW,uri);
 		startActivity(i);
 	}
+    //自动点击 对接tgsdk
+    public void auto(View v)
+    {
+       /* TGSDK.init(MainActivity.this,appid,null);
+        TGSDK.preload(MainActivity.this);
+        TGSDK.showTestView(sceneid);*/
+    }
+    //展示更多游戏
 	public void showothergames(View v)
 	{
-		Toast.makeText(ctx,"功能没有做",Toast.LENGTH_LONG).show();
+        Intent i=new Intent(ctx,MoreGame.class);
+        startActivity(i);
+		//Toast.makeText(ctx,"功能没有做",Toast.LENGTH_LONG).show();
 	}
 
     @Override
---
+++ src/main/java/com/littesandbox/clicksandbox/MoreGame.java
@@ -0,0 +1,46 @@
+package com.littesandbox.clicksandbox;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MoreGame extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.MoreGame);
+       
+    }
+    public void indienova(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW,Uri.parse("https://indienova.com/u/2439905184"));
+        startActivity(i); 
+    }
+    public void taptap(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.taptap.com/developer/83988"));
+        startActivity(i); 
+    }
+    //冲击
+    public void rush(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.taptap.com/app/189201"));
+        startActivity(i); 
+    }
+    public void fruit(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.taptap.com/app/196602"));
+        startActivity(i); 
+        }
+        public void music(View v)
+        {
+            Intent i=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.taptap.com/app/199683"));
+            startActivity(i); 
+        }
+    
+    
+    
+}
